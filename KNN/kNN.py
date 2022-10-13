60aa0eddc4013e64ba7db7708f8b6df58a79d4c4
董晓峰 <dongxiaofengxgdfz@163.com>
2022-10-13 12:40:59 -0500

    commit

---
+++ KNN/kNN.py
@@ -0,0 +1,87 @@
+from numpy import *
+import numpy as np
+import operator
+
+def createDataSet():
+    group = np.array([[1.0,1.1],[1.0,1.0],[0,0],[0,0.1]])
+    labels = ['A','A','B','B']
+    return group, labels
+
+
+def classify0(inX, dataSet, labels, k):
+    dataSetSize = dataSet.shape[0]
+    diffMat = np.tile(inX, (dataSetSize,1)) - dataSet  ## copy inX for dataSetSize times.
+    sqDiffMat = diffMat**2
+    sqDistances = sqDiffMat.sum(axis=1)
+    distances = sqDistances**0.5
+    sortedDistIndices = distances.argsort()
+    classCount = {}
+    for i in range(k):
+        voteIlabel = labels[sortedDistIndices[i]]
+        classCount[voteIlabel] = classCount.get(voteIlabel,0)+1  ### if doesn't exist, set to 1, if this class already exists in the dictionary, add one.
+    
+    sortedClassCount = sorted(classCount.items(), key=lambda x:x[1],reverse=True)
+    print(sortedClassCount)
+    return sortedClassCount[0][0]
+
+
+def file2matrix(filename):
+    love_dictionary={'largeDoses':3, 'smallDoses':2, 'didntLike':1}
+    fr = open(filename)
+    arrayOLines = fr.readlines()
+    numberOfLines = len(arrayOLines)            #get the number of lines in the file
+    returnMat = zeros((numberOfLines,3))        #prepare matrix to return
+    classLabelVector = []                       #prepare labels return   
+    index = 0
+    for line in arrayOLines:
+        line = line.strip()
+        listFromLine = line.split('\t')
+        returnMat[index,:] = listFromLine[0:3]
+        if(listFromLine[-1].isdigit()):
+            classLabelVector.append(int(listFromLine[-1]))
+        else:
+            classLabelVector.append(love_dictionary.get(listFromLine[-1]))
+        index += 1
+    return returnMat,classLabelVector
+
+
+def autoNorm(dataSet: np.ndarray) :
+
+    minVals = dataSet.min(0)  ### numpy.min method, 0 indicates axis=0, the first column. The min values along the first dimension.
+    maxVals = dataSet.max(0)
+    ranges = maxVals - minVals
+    normDataSet = zeros(shape(dataSet))
+    size = dataSet.shape[0]
+    normDataSet = dataSet - np.tile(minVals, (size,1))
+    normDataSet = normDataSet/np.tile(ranges, (size,1))   #element wise divide
+    return normDataSet, ranges, minVals
+
+
+
+def datingClassTest(Ratio=0.1, k=3):  ## k for how many nearest neighbors, Ratio for hold out ratio
+    hoRatio = Ratio   #hold out 10%
+    datingDataMat,datingLabels = file2matrix('datingTestSet2.txt')       #load data setfrom file
+    normMat, ranges, minVals = autoNorm(datingDataMat)
+    m = normMat.shape[0]
+    numTestVecs = int(m*hoRatio)  ## testing set size = holdoutratio * size of dataset.
+    errorCount = 0.0
+    for i in range(numTestVecs):
+        classifierResult = classify0(normMat[i,:],normMat[numTestVecs:m,:],datingLabels[numTestVecs:m],k)
+        print( "the classifier came back with: %d, the real answer is: %d" % (classifierResult, datingLabels[i]))
+        if (classifierResult != datingLabels[i]): errorCount += 1.0
+    print("the total error rate is: %f" % (errorCount/float(numTestVecs)))
+    print(errorCount)
+
+
+def classifyPerson():
+    resultList = ['not at all', 'in small doses', 'in large doses']
+    percentTats = float(input(\
+                                  "percentage of time spent playing video games?"))
+    ffMiles = float(input("frequent flier miles earned per year?"))
+    iceCream = float(input("liters of ice cream consumed per year?"))
+    datingDataMat, datingLabels = file2matrix('datingTestSet2.txt')
+    normMat, ranges, minVals = autoNorm(datingDataMat)
+    inArr = array([ffMiles, percentTats, iceCream, ])
+    classifierResult = classify0((inArr - \
+                                  minVals)/ranges, normMat, datingLabels, 3)
+    print("You will probably like this person: %s" % resultList[classifierResult - 1])

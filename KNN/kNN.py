0c69fbe568b990b5477a5fb95b5b7e7f858e74bc
董晓峰 <dongxiaofengxgdfz@163.com>
2022-10-13 14:02:18 -0500

    KNN complete

--- KNN/kNN.py
+++ KNN/kNN.py
@@ -1,5 +1,6 @@
 from numpy import *
 import numpy as np
+from os import listdir
 import operator
 
 def createDataSet():
@@ -85,3 +86,39 @@ def classifyPerson():
     classifierResult = classify0((inArr - \
                                   minVals)/ranges, normMat, datingLabels, 3)
     print("You will probably like this person: %s" % resultList[classifierResult - 1])
+
+
+
+def img2vector(filename):
+    returnVect = zeros((1,1024))
+    fr = open(filename)
+    for i in range(32):
+        lineStr = fr.readline()
+        for j in range(32):
+            returnVect[0,32*i+j] = int(lineStr[j])
+    return returnVect
+
+def handwritingClassTest():
+    hwLabels = []
+    trainingFileList = listdir('./digits/trainingDigits')           #load the training set
+    m = len(trainingFileList)
+    trainingMat = zeros((m,1024))
+    for i in range(m):
+        fileNameStr = trainingFileList[i]
+        fileStr = fileNameStr.split('.')[0]     #take off .txt
+        classNumStr = int(fileStr.split('_')[0])
+        hwLabels.append(classNumStr)
+        trainingMat[i,:] = img2vector('./digits/trainingDigits/%s' % fileNameStr)
+    testFileList = listdir('./digits/testDigits')        #iterate through the test set
+    errorCount = 0.0
+    mTest = len(testFileList)
+    for i in range(mTest):
+        fileNameStr = testFileList[i]
+        fileStr = fileNameStr.split('.')[0]     #take off .txt
+        classNumStr = int(fileStr.split('_')[0])
+        vectorUnderTest = img2vector('./digits/testDigits/%s' % fileNameStr)
+        classifierResult = classify0(vectorUnderTest, trainingMat, hwLabels, 3)
+        print("the classifier came back with: %d, the real answer is: %d" % (classifierResult, classNumStr))
+        if (classifierResult != classNumStr): errorCount += 1.0
+    print("\nthe total number of errors is: %d" % errorCount)
+    print("\nthe total error rate is: %f" % (errorCount/float(mTest)))
